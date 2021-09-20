5b815f4cb4f21d10292e32c9b6c69d8bfec01ee0
Godbless <webnaimur@gmail.com>
2021-09-20 23:41:40 +0600

    topbar mission add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './component/header/sub-component/nav-bar/Navbar';
+import OurMission from './component/our-mission/OurMission';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar/>
+      <OurMission/>
     </div>
   )
 }
