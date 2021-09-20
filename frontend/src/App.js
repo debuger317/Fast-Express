d95b65db7985e472e7fac75546533abc3c8e130c
rayhanwd <rayhanbd4400@gmail.com>
2021-09-20 19:00:14 +0600

    Initilaze our Team Project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './component/header/sub-component/nav-bar/Navbar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App;
