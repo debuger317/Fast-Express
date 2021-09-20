d7bb5aece4c8f043a833a1e805dc555ecac62e8e
Abdul Hannan <shohas563@gmail.com>
2021-09-20 21:53:03 +0600

    review & support created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from './component/header/sub-component/nav-bar/Navbar';
+import Support from './component/support/Support';
+import Review from './component/reviews/Review.jsx';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar/>
+      <Support/>
+      <Review/>
     </div>
   )
 }
---
+++ frontend/src/component/reviews/Review.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+const Review = () => {
+    return (
+        <div class="mt-5">
+            <p class="text-center text-3xl font-bold text-gray-800 dark:text-white">
+                Client Review
+            </p>
+            <p class="text-center mb-12 text-xl font-normal text-gray-500 dark:text-gray-200">
+                Meat the best Client in the World
+            </p>
+
+            <div class="w-full flex flex-col md:flex-row gap-4 mb-8 md:mb-0 flex-between items-center p-8">
+                <div class="bg-white dark:bg-gray-800 w-72 shadow-lg mx-auto rounded-xl p-4">
+                    <p class="text-gray-600 dark:text-white">
+                        <span class="font-bold text-indigo-500 text-lg">
+                            “
+                        </span>
+                        To get social media testimonials like these, keep your customers engaged with your social media accounts by posting regularly yourself
+                        <span class="font-bold text-indigo-500 text-lg">
+                            ”
+                        </span>
+                    </p>
+                    <div class="flex items-center mt-4">
+                        <a href="#" class="block relative">
+                            <img alt="profil" src="/images/person/1.jpg" class="mx-auto object-cover rounded-full h-10 w-10 " />
+                        </a>
+                        <div class="flex flex-col ml-2 justify-between">
+                            <span class="font-semibold text-indigo-500 text-sm">
+                                Jean Miguel
+                            </span>
+                            <span class="dark:text-gray-400 text-xs flex items-center">
+                                User of Tail-Kit
+                                <img src="/icons/rocket.svg" class="ml-2 h-4 w-4" />
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white dark:bg-gray-800 w-72 shadow-lg mx-auto rounded-xl p-4">
+                    <p class="text-gray-600 dark:text-white">
+                        <span class="font-bold text-indigo-500 text-lg">
+                            “
+                        </span>
+                        To get social media testimonials like these, keep your customers engaged with your social media accounts by posting regularly yourself
+                        <span class="font-bold text-indigo-500 text-lg">
+                            ”
+                        </span>
+                    </p>
+                    <div class="flex items-center mt-4">
+                        <a href="#" class="block relative">
+                            <img alt="profil" src="/images/person/1.jpg" class="mx-auto object-cover rounded-full h-10 w-10 " />
+                        </a>
+                        <div class="flex flex-col ml-2 justify-between">
+                            <span class="font-semibold text-indigo-500 text-sm">
+                                Jean Miguel
+                            </span>
+                            <span class="dark:text-gray-400 text-xs flex items-center">
+                                User of Tail-Kit
+                                <img src="/icons/rocket.svg" class="ml-2 h-4 w-4" />
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white dark:bg-gray-800 w-72 shadow-lg mx-auto rounded-xl p-4">
+                    <p class="text-gray-600 dark:text-white">
+                        <span class="font-bold text-indigo-500 text-lg">
+                            “
+                        </span>
+                        To get social media testimonials like these, keep your customers engaged with your social media accounts by posting regularly yourself
+                        <span class="font-bold text-indigo-500 text-lg">
+                            ”
+                        </span>
+                    </p>
+                    <div class="flex items-center mt-4">
+                        <a href="#" class="block relative">
+                            <img alt="profil" src="/images/person/1.jpg" class="mx-auto object-cover rounded-full h-10 w-10 " />
+                        </a>
+                        <div class="flex flex-col ml-2 justify-between">
+                            <span class="font-semibold text-indigo-500 text-sm">
+                                Jean Miguel
+                            </span>
+                            <span class="dark:text-gray-400 text-xs flex items-center">
+                                User of Tail-Kit
+                                <img src="/icons/rocket.svg" class="ml-2 h-4 w-4" />
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Review;
