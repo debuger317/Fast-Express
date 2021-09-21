12eca3b74bc0c568b8873149229a2fa31e65d1a6
rayhanwd <rayhanbd4400@gmail.com>
2021-09-22 00:28:01 +0600

    updated

--- frontend/src/component/faq/FAQ.jsx
+++ frontend/src/component/faq/FAQ.jsx
@@ -16,32 +16,33 @@ const FAQ = () => {
    }
    return (
       <section>
-<div className="w-1/1 md:w-3/5 mx-auto p-8">
-         <p>Open <strong>one</strong></p>
-         <div className="shadow-md">
-            <div className="tab w-full overflow-hidden border-t">
-               <input className="absolute opacity-0" id="tab-single-one" type="radio" name="tabs2"/>
-               <label className="block p-5 leading-normal cursor-pointer" for="tab-single-one">Label One</label>
-               <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                  <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+         <div className="w-1/1 md:w-3/5 mx-auto p-8">
+            <p className="text-5xl">Who We Are</p>
+            <p>Know more about us and our cause</p>
+            <div className="shadow-md">
+               <div className="tab w-full overflow-hidden border-t">
+                  <input className="absolute opacity-0" id="tab-single-one" type="radio" name="tabs2" />
+                  <label className="block p-5 leading-normal cursor-pointer" for="tab-single-one">Label One</label>
+                  <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+                     <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+                  </div>
                </div>
-            </div>
-            <div className="tab w-full overflow-hidden border-t">
-               <input className="absolute opacity-0" id="tab-single-two" type="radio" name="tabs2"/>
-               <label className="block p-5 leading-normal cursor-pointer" for="tab-single-two">Label Two</label>
-               <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                  <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+               <div className="tab w-full overflow-hidden border-t">
+                  <input className="absolute opacity-0" id="tab-single-two" type="radio" name="tabs2" />
+                  <label className="block p-5 leading-normal cursor-pointer" for="tab-single-two">Label Two</label>
+                  <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+                     <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+                  </div>
                </div>
-            </div>
-            <div className="tab w-full overflow-hidden border-t">
-               <input className="absolute opacity-0" id="tab-single-three" type="radio" name="tabs2"/>
-               <label className="block p-5 leading-normal cursor-pointer" for="tab-single-three">Label Three</label>
-               <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                  <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+               <div className="tab w-full overflow-hidden border-t">
+                  <input className="absolute opacity-0" id="tab-single-three" type="radio" name="tabs2" />
+                  <label className="block p-5 leading-normal cursor-pointer" for="tab-single-three">Label Three</label>
+                  <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+                     <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
+                  </div>
                </div>
             </div>
          </div>
-      </div>
       </section>
    )
 }
