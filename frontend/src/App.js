7deb904341ec2dbb3052d28d349f13d4e2d073ae
rayhanwd <rayhanbd4400@gmail.com>
2021-09-21 11:30:09 +0600

    faq and promoSell

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import FAQ from './component/faq/FAQ';
 import Navbar from './component/header/sub-component/nav-bar/Navbar';
-
+import PromoSell from './component/promo-sell/PromoSell.jsx';
 const App = () => {
   return (
     <div className="App">
       <Navbar/>
+      <FAQ />
+      <PromoSell />
     </div>
   )
 }
