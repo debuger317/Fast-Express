9e3e6828237aea7be8302591f47621c563a438eb
rayhanwd <rayhanbd4400@gmail.com>
2021-09-21 21:50:39 +0600

    fixed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,14 +4,16 @@ import Footer from '../component/footer/Footer';
 import OurMission from '../component/our-mission/OurMission';
 import Support from '../component/support/Support';
 import Review from '../component/reviews/Review';
-
+import FAQ from '../component/faq/FAQ';
+import PromoSell from '../component/promo-sell/PromoSell';
 const Home = () => {
     return (
         <Fragment>
             <Header />
             <OurMission />
             <Support />
-    
+            <FAQ />
+            <PromoSell />
             <Review />
             <Footer/>
         </Fragment>
