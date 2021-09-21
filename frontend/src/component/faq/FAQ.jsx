38841ee92c265d9627b5136014437523e43f637e
rayhanwd <rayhanbd4400@gmail.com>
2021-09-21 15:58:13 +0600

    fixed faq

--- frontend/src/component/faq/FAQ.jsx
+++ frontend/src/component/faq/FAQ.jsx
@@ -16,48 +16,32 @@ const FAQ = () => {
    }
    return (
       <section>
-
-         <div className="w-full md:w-3/5 mx-auto p-8">
-            <h1>Who We Are</h1>
-            <p>Know more about us and <strong> our cause</strong></p>
-            <div className="shadow-md">
-               <div className="tab w-full overflow-hidden border-t">
-                  <input className="absolute opacity-0 " id="tab-multi-one" type="checkbox" name="tabs" />
-                  <label className="block p-5 leading-normal cursor-pointer" for="tab-multi-one">WHAT DO WE DO?</label>
-                  <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                     <p className="p-5">Fast Express is a platform that matches you to a nearby driver to complete your delivery.</p>
-                  </div>
+<div className="w-1/1 md:w-3/5 mx-auto p-8">
+         <p>Open <strong>one</strong></p>
+         <div className="shadow-md">
+            <div className="tab w-full overflow-hidden border-t">
+               <input className="absolute opacity-0" id="tab-single-one" type="radio" name="tabs2"/>
+               <label className="block p-5 leading-normal cursor-pointer" for="tab-single-one">Label One</label>
+               <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+                  <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
                </div>
-               <div className="tab w-full overflow-hidden border-t">
-                  <input className="absolute opacity-0" id="tab-multi-two" type="checkbox" name="tabs" />
-                  <label className="block p-5 leading-normal cursor-pointer" for="tab-multi-two">WHO AER OUR DELEVERS</label>
-                  <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                     <p className="p-5">They are certified drivers and riders from our community who have signed up with us.</p>
-                  </div>
+            </div>
+            <div className="tab w-full overflow-hidden border-t">
+               <input className="absolute opacity-0" id="tab-single-two" type="radio" name="tabs2"/>
+               <label className="block p-5 leading-normal cursor-pointer" for="tab-single-two">Label Two</label>
+               <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+                  <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
                </div>
-               <div className="tab w-full overflow-hidden border-t">
-                  <input className="absolute opacity-0" id="tab-multi-three" type="checkbox" name="tabs" />
-                  <label className="block p-5 leading-normal cursor-pointer" for="tab-multi-three">WHAT MAKE FAST EXPRESS EFFICIENT</label>
-                  <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                     <p className="p-5">We do not take a commission from the sales. 100% of the sales goes to the drivers. Drivers are more willing and eager to complete your deliveries. Which translates to higher acceptance and performance rate.</p>
-                  </div>
-               </div>
-               <div className="tab w-full overflow-hidden border-t">
-                  <input className="absolute opacity-0" id="tab-multi-two" type="checkbox" name="tabs" />
-                  <label className="block p-5 leading-normal cursor-pointer" for="tab-multi-two">WHO AER OUR DELEVERS</label>
-                  <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                     <p className="p-5">Most companies earns from commission. That adds up hundreds to thousands of dollars of hard earned money being deprived per month to the driver. We charge only $1-$2 per day or just $30-$60 per month to the driver to maintain our application.</p>
-                  </div>
-               </div>
-               <div className="tab w-full overflow-hidden border-t">
-                  <input className="absolute opacity-0" id="tab-multi-two" type="checkbox" name="tabs" />
-                  <label className="block p-5 leading-normal cursor-pointer" for="tab-multi-two">HOW DOES FAST EXPRESS MANTAIN THIS APPLICATION AND WHAT IS OUR CAUSE</label>
-                  <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
-                     <p className="p-5">They are certified drivers and riders from our community who have signed up with us.</p>
-                  </div>
+            </div>
+            <div className="tab w-full overflow-hidden border-t">
+               <input className="absolute opacity-0" id="tab-single-three" type="radio" name="tabs2"/>
+               <label className="block p-5 leading-normal cursor-pointer" for="tab-single-three">Label Three</label>
+               <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+                  <p className="p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum adipisci eum? Saepe, itaque.</p>
                </div>
             </div>
          </div>
+      </div>
       </section>
    )
 }
