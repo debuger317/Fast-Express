b6b3ead9242e7abce828cfb4ce2f8cc82e4e3506
AMFahim <amfahim2019@gmail.com>
2021-09-21 21:13:03 +0600

    swiper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './component/header/sub-component/nav-bar/Navbar';
 import Support from './component/support/Support';
 import Review from './component/reviews/Review.jsx';
 import OurMission from './component/our-mission/OurMission';
+import FeatureBrand from './component/feature-brand/FeatureBrand';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <OurMission/>
       <Support/>
       <Review/>
+      <FeatureBrand/>
       <Footer/>
     </div>
   )
--- frontend/src/component/feature-brand/FeatureBrand.jsx
+++ frontend/src/component/feature-brand/FeatureBrand.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FeatureBrand = () => {
     return (
         <div>
-            Feature Brand
+        Feature Brand
         </div>
     )
 }
