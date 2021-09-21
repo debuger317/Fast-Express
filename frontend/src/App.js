55100a317b85d894f67cd49fd6e01bb267296a85
rayhanwd <rayhanbd4400@gmail.com>
2021-09-21 09:22:07 +0600

    resolved code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Footer from './component/footer/Footer';
 import Navbar from './component/header/sub-component/nav-bar/Navbar';
 import Support from './component/support/Support';
 import Review from './component/reviews/Review.jsx';
+import OurMission from './component/our-mission/OurMission';
 
 const App = () => {
   return (
