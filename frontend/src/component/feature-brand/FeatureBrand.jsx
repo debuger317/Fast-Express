855ea10069ab3414d107122bd6fdbd2bc67ea47c
AMFahim <amfahim2019@gmail.com>
2021-09-22 00:14:47 +0600

    feature brand

--- frontend/src/component/feature-brand/FeatureBrand.jsx
+++ frontend/src/component/feature-brand/FeatureBrand.jsx
@@ -20,31 +20,31 @@ const FeatureBrand = () => {
           "clickable": true
         }} navigation={true} className="mySwiper justify-items-center">
           <SwiperSlide>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_BPkPBRCOGxs_ZRnEALwGAE1ODfcCDIp_Eg&usqp=CAU" alt="curior" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_BPkPBRCOGxs_ZRnEALwGAE1ODfcCDIp_Eg&usqp=CAU" alt="currier" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDdTaTX5pV2sp-RbRrNk9sgxXfNzaqM1y03g&usqp=CAU" alt="courier" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDdTaTX5pV2sp-RbRrNk9sgxXfNzaqM1y03g&usqp=CAU" alt="currier" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5GyPZsMgguw24Fs1Qi6ac0GfbM0yNuGkUug&usqp=CAU" alt="courier" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5GyPZsMgguw24Fs1Qi6ac0GfbM0yNuGkUug&usqp=CAU" alt="currier" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTjRd_Z3iky5HeK9cBnNwHZz8YNFBwzOPXIg&usqp=CAU" alt="Courier" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTjRd_Z3iky5HeK9cBnNwHZz8YNFBwzOPXIg&usqp=CAU" alt="currier" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLHa4XwXjj219B7tJ4VburzssJq0ckiE--4w&usqp=CAU" alt="Courier" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLHa4XwXjj219B7tJ4VburzssJq0ckiE--4w&usqp=CAU" alt="currier" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV8fac58tbH5l18AR1j2JV9-qCGifChwugdQ&usqp=CAU" alt="Courier " />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV8fac58tbH5l18AR1j2JV9-qCGifChwugdQ&usqp=CAU" alt="currier " />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD_Bdja_mp1v_CEhGqn80wzIvYkpNdlUxtlw&usqp=CAU" alt="Courier company" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD_Bdja_mp1v_CEhGqn80wzIvYkpNdlUxtlw&usqp=CAU" alt="currier company" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK7CRNTCeAkbrsqZDqHMa_v8hfJJKoZ8MNpw&usqp=CAU" alt="Courier Company" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK7CRNTCeAkbrsqZDqHMa_v8hfJJKoZ8MNpw&usqp=CAU" alt="currier Company" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8HshjBatbfrBMLzgB3uPzhpEUDl_mM5gULg&usqp=CAU" alt="Courier Company" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8HshjBatbfrBMLzgB3uPzhpEUDl_mM5gULg&usqp=CAU" alt="currier Company" />
           </SwiperSlide>
         </Swiper>
       </div>
