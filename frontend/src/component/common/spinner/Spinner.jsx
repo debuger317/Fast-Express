6adb35d23b5779ae3905f376300b28488d76574e
Abdul Hannan <shohas563@gmail.com>
2021-09-26 10:35:24 +0600

    card design

--- frontend/src/component/common/spinner/Spinner.jsx
+++ frontend/src/component/common/spinner/Spinner.jsx
@@ -5,8 +5,7 @@ const Spinner = () => {
         <div className="container mx-auto">
             <span className="flex">
                 <span className="animate-ping absolute inline-flex h-full w-full bg-purple-400 opacity-75"></span>
-                <span className="relative inline-flex  h-full w-full">
-                </span>
+                <span className="relative inline-flex  h-full w-full"></span>
             </span>
         </div>
     )
