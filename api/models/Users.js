e9d4d7874508b6019e7bc96bc852cafe8e3740ff
rayhanwd <rayhanbd4400@gmail.com>
2021-09-29 11:27:16 +0600

    updated user

--- api/models/Users.js
+++ api/models/Users.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema({
     photo: {
         type: String,
         default: "",
-        unique: true,
+        required: false,
     },
     label: {
         type: String,
