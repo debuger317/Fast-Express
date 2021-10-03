ebfe2c9bbb9833a7ef57f89896bbbd0dd9192e45
rayhanwd <rayhanbd4400@gmail.com>
2021-10-04 00:56:50 +0600

    api code modified

---
+++ api/controllers/customers.js
@@ -0,0 +1,34 @@
+const customers = require('../models/customers');
+
+const customer_add_c = async (req, res) => {
+
+    try {
+        const newCustomer = await new customers({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            photo: req.body.photo,
+            label: req.body.label,
+        })
+        const customer = await newCustomer.save();
+        res.status(200).send(customer);
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const getallcustomer_C = async (req, res) => {
+    try {
+        const customer = await customers.find();
+        res.status(200).json(customer);
+    }
+    catch (err) { res.status(500).json(err) }
+}
+
+module.exports = {
+    customer_add_c,
+    getallcustomer_C
+}
+
--- api/routes/customers.js
+++ api/routes/customers.js
@@ -1,30 +1,8 @@
 const router = require('express').Router();
-const customers = require('../models/customers');
+const customers_c = require('../controllers/customers');
 
-router.post('/register', async (req, res) => {
-    try {
-        const newCustomer = await new customers({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            photo: req.body.photo,
-            label: req.body.label,
-        })
-        const customer = await newCustomer.save();
-        res.status(200).send(customer);
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).json({ message: err.message });
-    }
-})
+router.post('/register',customers_c.customer_add_c)
 
-router.get('/all', async (req, res) => {
-    try {
-        const customer = await customers.find();
-        res.status(200).json(customer);
-    }
-    catch (err) { res.status(500).json(err) }
-})
+router.get('/all',customers_c.getallcustomer_C)
 
 module.exports = router;
