fbdb72473613055859491d838b7dac569253b67d
rayhanwd <rayhanbd4400@gmail.com>
2021-10-03 12:28:17 +0600

    discount section added

--- frontend/src/components/promo-sell/PromoSell.jsx
+++ frontend/src/components/promo-sell/PromoSell.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 
 const PromoSell = () => {
     return (
-        <section classNameName="container  mx-auto">
 
-            <div className="flex mx-auto">
-              
-                <div class="w-6/12 relative ">
-                    <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg" alt="" class=" object-center object-cover" />
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto flex flex-wrap">
+                <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+                    <img class="object-cover object-center h-full w-full" src="https://i.ibb.co/0BQvvWh/promo-sell.jpg" />
                 </div>
-                <div class="relative w-6/12 bg-white dark:bg-gray-800 p-4">
-                    <h2>FEATURE AND MENTIONED ON</h2>
+                <div class=" lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left mx-auto  text-center">
+
+                    <h1 class="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl">
+                        Summer styles are finally here
+                    </h1>
+                    <p class="mt-4 text-xl text-gray-500">This year, our new summer collection will shelter you from the harsh elements of a world that doesn't care if you live or die.</p>
+                    <button className="bg-primary text-xl  f-20">Order Now</button>
+
                 </div>
             </div>
-
-
-
         </section>
+
 
 
 
