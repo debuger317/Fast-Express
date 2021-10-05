d47422de9d4766cfc58a7228c900960bdce2ccbc
Abdul Hannan <shohas563@gmail.com>
2021-10-05 11:55:04 +0600

    ksjiehig

--- frontend/src/components/auth/PrivateRoute.js
+++ frontend/src/components/auth/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect, Route } from 'react-router';
 import { useSelector } from 'react-redux';
 const PrivateRoute = ({ children, ...rest }) => {
-  const email = useSelector((state) => state.auth.userEmail)
+  const email = useSelector((state) => state.auth.userdetails.email)
   console.log(email);
     return (
         <Route
