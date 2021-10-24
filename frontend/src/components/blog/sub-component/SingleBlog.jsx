06c22321027e0bcc33eecb1a5f1ecb367b0ab983
Zihadul Islam <zihadul10101@gmail.com>
2021-10-24 23:08:59 +0600

    single blog added

---
+++ frontend/src/components/blog/sub-component/SingleBlog.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import HappyDelivery from '../../../assets/images/blog/Happy-Deepavali-Whatsapp-Story.png'
+const SingleBlog = () => {
+    return (
+        <div>
+            <section class="text-gray-600 body-font">
+                <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={HappyDelivery} />
+                    <div class="text-center lg:w-2/3 w-full">
+                        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">FROM $8.00 per location Bulk delivery for Christmas & New year Hamper</h1>
+                        <p class="mb-8 leading-relaxed">We are taking orders for hamper delivery. Book early to get promotion rates, kale chips beard jianbing tousled.Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh.Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork
+                            Book early to get promotion rates, kale chips beard jianbing tousled.Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh.Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+
+                        <h1 class="title-font sm:text-4xl text-3xl mb-4 pt-5 font-medium text-gray-900">Related Blogs</h1>
+                        <div class="flex flex-wrap justify-center">
+                            <div class="p-4 lg:w-1/2">
+                                <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                                    <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={HappyDelivery} />
+                                    <div class="flex-grow sm:pl-8">
+                                        <h2 class="title-font font-medium text-lg text-gray-900">Holden Caulfield</h2>
+                                        <h3 class="text-gray-500 mb-3">UI Developer</h3>
+                                        <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                        <span class="inline-flex">
+                                            <a class="text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a class="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a class="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                                </svg>
+                                            </a>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-4 lg:w-1/2">
+
+                                <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                                    <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={HappyDelivery} />
+                                    <div class="flex-grow sm:pl-8">
+                                        <h2 class="title-font font-medium text-lg text-gray-900">Holden Caulfield</h2>
+                                        <h3 class="text-gray-500 mb-3">UI Developer</h3>
+                                        <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                        <span class="inline-flex">
+                                            <a class="text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a class="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a class="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                                </svg>
+                                            </a>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    );
+};
+
+export default SingleBlog;
