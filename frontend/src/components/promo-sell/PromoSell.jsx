8393ec88c6524d4f90e704d683fd818b31c6b18b
Abdul Hannan <shohas563@gmail.com>
2021-10-31 20:27:41 +0600

    front service pdate

--- frontend/src/components/promo-sell/PromoSell.jsx
+++ frontend/src/components/promo-sell/PromoSell.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PromoSell = () => {
     return (
@@ -10,10 +11,10 @@ const PromoSell = () => {
                 <div class="lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left mx-auto flex items-center">
                     <div>
                         <h1 class="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl pb-5">
-                            Summer styles are finally here
+                            Doot stpe service is finally here
                         </h1>
                         <p class="mt-4 text-xl text-gray-500 pb-5">This year, our new summer collection will shelter you from the harsh elements of a world that doesn't care if you live or die.This year, our new summer collection will shelter you from the harsh elements of a world that doesn't care if you live or die.</p>
-                        <button className="bg-red-600 rounded text-xl text-white px-5 py-2 hover:bg-red-700">Order Now</button>
+                        <Link to="/service"><button className="bg-red-600 rounded text-xl text-white px-5 py-2 hover:bg-red-700">Order Now</button></Link>
 
                     </div>
                 </div>
